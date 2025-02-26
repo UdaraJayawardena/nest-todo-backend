c486b256233f063b28dcbc13bf2b45e75ae9426a
Udara Jayawardena <udarajayawardena5@gmail.com>
2025-02-26 20:16:57 +0530

    feat: create multiple schema  models

---
+++ prisma/merge/merge-schema.js
@@ -0,0 +1,29 @@
+// merge-schema.js
+
+const fs = require('fs');
+
+// Read the contents from multiple schema files
+const userSchema = fs.readFileSync('prisma/models/user.prisma', 'utf8');
+const todoSchema = fs.readFileSync('prisma/models/todo.prisma', 'utf8');
+
+// Merge the schemas and update the schema.prisma file
+const finalSchema = `
+
+generator client {
+  provider = "prisma-client-js"
+}
+
+datasource db {
+  provider = "postgresql"
+  url      = env("DATABASE_URL")
+}
+
+${userSchema}
+
+${todoSchema}
+`;
+
+// Write the merged schema into the main schema.prisma
+fs.writeFileSync('prisma/schema/schema.prisma', finalSchema);
+
+console.log("=== Schemas merged into schema.prisma file ===");
